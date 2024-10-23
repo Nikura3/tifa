bded6428c86a03bcf0abef1cd5e66941047a9bff
davidevezzaro <davide.vezzaro1@gmail.com>
2024-10-23 17:58:23 +0200

    path walking to retrieve generated images

---
+++ config.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Dict, List
+
+
+@dataclass
+class RunConfig:
+    # Guiding text prompt
+    image_path: Path = Path('evaluation','QBench')
+    prompt_collection_path: Path = Path('prompt_collection','QBench')
+    qa_model: str = 'allenai/unifiedqa-v2-t5-large-1363200'
+    vqa_model: str = 'mplug-large'
+    gpt_model: str= 'tifa-benchmark/llama2_tifa_question_generation'
--- tifa_test.py
+++ tifa_test.py
@@ -1,63 +1,70 @@
 from tifascore import get_question_and_answers, filter_question_and_answers, UnifiedQAModel, tifa_score_benchmark, tifa_score_single,  VQAModel
 from tifascore import get_llama2_pipeline, get_llama2_question_and_answers
 import json
-#import openai
+from config import RunConfig
+import os
 
+def readCSV(prompt_collection_path):
+    return [("001","A bus"), ("002","A bus and a bench")]
 
-def main():
-    print("a")
+def main(config : RunConfig):
+    """ #Load the models
+    unifiedqa_model = UnifiedQAModel(config.qa_model)
+    vqa_model = VQAModel(config.vqa_model)
+    #llama2 for local gpt model, from Hugging Face
+    pipeline = get_llama2_pipeline(config.gpt_model) """
 
+    if not (os.path.isdir(config.image_path)):
+        print("Prompt collection not found!")
+    else:
+        """ # Walk through the directory and subdirectories
+        for dirpath, dirnames, filenames in os.walk(config.image_path):
+            for file in filenames:
+                if not file.endswith((".csv", ".png")): #to delete csv files and bounding box version of the image
+                    file_paths.append((dirpath, file))
+        """
+        #TODO ADD SEQUENTIAL NUMBER IN EACH PROMPT!
+        models_to_evaluate = []
+        for model in os.listdir(config.image_path):
+            if(os.path.isdir((os.path.join(config.image_path,model)))):
+                models_to_evaluate.append((os.path.join(config.image_path,model), model))
+
+        prompts_to_evaluate = readCSV(config.prompt_collection_path)
+
+        images = []
+        for model in models_to_evaluate:
+            for prompt in prompts_to_evaluate:
+                root_img_path = os.path.join(model[0],prompt[1])
+                prompt = prompt[1]
+                for file in os.listdir(root_img_path):
+                    if not file.endswith((".csv",".png")):
+                        if(os.path.isfile(os.path.join(root_img_path,file))):
+                            images.append((root_img_path,prompt,file))
+
+        # root_img_path, prompt, image_filename
+        images.sort(key=lambda x: (x[0], x[1], int(x[2].split('.')[0])))
+
+        """ llama2_questions = get_llama2_question_and_answers(pipeline, text)
+        
+        # Filter questions with UnifiedQA
+        #filtered_questions = filter_question_and_answers(unifiedqa_model, gpt3_questions)
+        
+        # Filter questions with UnifiedQA
+        filtered_questions = filter_question_and_answers(unifiedqa_model, llama2_questions)
+
+        # See the questions
+        #print(filtered_questions)
+
+        # calculate TIFA score
+        result = tifa_score_single(vqa_model, filtered_questions, img_path)
+
+        print("-- tifa_score: --")
+        print(result['tifa_score'])
+        print("-- question details: --")
+        print(result['question_details']) """
 
 if __name__ == "__main__":
-    
-    #####################################
-    ## Test TIFA score on benchmark
-    #####################################
-    
-    # test tifa benchmarking
-    """ results = tifa_score_benchmark("mplug-large", "sample/sample_question_answers.json", "sample/sample_imgs.json")
-    with open("sample/sample_evaluation_result.json", "w") as f:
-        json.dump(results, f, indent=4)  """
-    
-    
-    #####################################
-    ## Test TIFA score on one image
-    #####################################
-    
-    # prepare the models
-    #openai.api_key = "[OpenAI key]"
-    unifiedqa_model = UnifiedQAModel("allenai/unifiedqa-v2-t5-large-1363200")
-    vqa_model = VQAModel("mplug-large")
 
-    img_path = "evaluation/1.jpg"
-    text = "A bus and a bench"
-    
-    # Generate questions with GPT-3.5-turbo
-    #gpt3_questions = get_question_and_answers(text)
-    #print("-----------------------Questions------------------\n",gpt3_questions)
-
-    # Generate questions with llama_2
-    pipeline = get_llama2_pipeline("tifa-benchmark/llama2_tifa_question_generation")
-    llama2_questions = get_llama2_question_and_answers(pipeline, text)
-    
-    # Filter questions with UnifiedQA
-    #filtered_questions = filter_question_and_answers(unifiedqa_model, gpt3_questions)
-    
-    # Filter questions with UnifiedQA
-    filtered_questions = filter_question_and_answers(unifiedqa_model, llama2_questions)
-
-    # See the questions
-    #print(filtered_questions)
-
-    # calculate TIFA score
-    result = tifa_score_single(vqa_model, filtered_questions, img_path)
-    
-    #print(f"TIFA score is {result['tifa_score']}")
-    
-    print(result.keys())
-    print("-- tifa_score: --")
-    print(result['tifa_score'])
-    print("-- question details: --")
-    print(result['question_details'])
+    main(RunConfig())
     
     
