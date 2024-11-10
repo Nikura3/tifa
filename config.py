31112d8fd0abaff15e7de616224b4e7cd532f6d8
davidevezzaro <davide.vezzaro1@gmail.com>
2024-11-10 15:10:26 +0100

    minor change

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ class TifaVersion(Enum): REGULAR = "REGULAR"; EXTENDED = "EXTENDED"
 @dataclass
 class RunConfig:
     # Guiding text prompt
-    eval_path: Path = Path('evaluation','QBench')
+    prompt_collection ='QBench'
+    eval_path: Path = Path('evaluation',prompt_collection)
     qa_model: str = 'allenai/unifiedqa-v2-t5-large-1363200'
     vqa_model: str = 'mplug-large'
     gpt_model: str= 'tifa-benchmark/llama2_tifa_question_generation'
